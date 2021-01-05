f232bc696d7e773252326105f8be2d54a1de4288
Tien Dat <datnt@tienngay.vn>
2021-01-05 10:55:21 +0700

    master

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import Navigator from './src/navigator/index';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Navigator);
+AppRegistry.registerComponent(appName, () => App);
